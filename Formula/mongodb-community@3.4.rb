d5160e7ff0ab742dc830711ed893d38041f2812a
Mathew Robinson <chasinglogic@gmail.com>
2019-09-16 16:15:29 -0400

    SERVER-43358 Update Homebrew formula to 3.4.23

--- Formula/mongodb-community@3.4.rb
+++ Formula/mongodb-community@3.4.rb
@@ -4,7 +4,7 @@ class MongodbCommunityAT34 < Formula
 
   # frozen_string_literal: true
 
-  url "https://fastdl.mongodb.org/osx/mongodb-osx-ssl-x86_64-3.4.22.tgz"
+  url "https://fastdl.mongodb.org/osx/mongodb-osx-ssl-x86_64-3.4.23.tgz"
   sha256 "653251ff57a55f513b58933b278f5a5cfc6bd90aa5d52ec87661ae635c413647"
 
   bottle :unneeded
