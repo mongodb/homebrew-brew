66de25e5fa468a90303abf7e83450e49566939ba
mcasimir <maurizio.cas@gmail.com>
2020-06-05 15:30:27 +0200

    release mongosh 0.0.5

--- Formula/mongosh.rb
+++ Formula/mongosh.rb
@@ -3,8 +3,8 @@ require "language/node"
 class Mongosh < Formula
   desc "The MongoDB Shell"
   homepage "https://github.com/mongodb-js/mongosh#readme"
-  url "https://registry.npmjs.org/@mongosh/cli-repl/-/cli-repl-0.0.1.tgz"
-  version "0.0.1"
+  url "https://registry.npmjs.org/@mongosh/cli-repl/-/cli-repl-0.0.5.tgz"
+  version "0.0.5"
   sha256 "70aab0f585cb4fc0c1e2c26bbe19d36d2d68022fe2cca85fe8099196a9cd7606"
 
   depends_on "node"
