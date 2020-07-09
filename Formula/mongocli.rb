ee7b4e9611d0f7963c43d9df098c3512166bdddd
goreleaserbot <goreleaser@carlosbecker.com>
2020-07-09 12:03:48 +0100

    Brew formula update for mongocli version v1.1.0

--- Formula/mongocli.rb
+++ Formula/mongocli.rb
@@ -2,21 +2,24 @@
 class Mongocli < Formula
   desc "The MongoDB Command Line Interface (mongocli) is a tool for managing your MongoDB cloud services, like MongoDB Atlas, MongoDB Cloud Manager, and MongoDB Ops Manager."
   homepage "https://github.com/mongodb/mongocli"
-  version "1.0.0"
+  version "1.1.0"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/mongodb/mongocli/releases/download/v1.0.0/mongocli_1.0.0_macos_x86_64.zip"
-    sha256 "bd47d06a54c5f95c26b0729d9175800894ebb5c944d031b2c6e941c9afb1dfb9"
+    url "https://github.com/mongodb/mongocli/releases/download/v1.1.0/mongocli_1.1.0_macos_x86_64.zip"
+    sha256 "7e75a01fdea6f594b0ce069efdeaeaa3b45096cd65d642815b17c72e1b5730e1"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/mongodb/mongocli/releases/download/v1.0.0/mongocli_1.0.0_linux_x86_64.tar.gz"
-      sha256 "8463461bb460b066097e5e6e229a4ffcd30db1f68605de11c75507b9a3a51234"
+      url "https://github.com/mongodb/mongocli/releases/download/v1.1.0/mongocli_1.1.0_linux_x86_64.tar.gz"
+      sha256 "d1280e618faf27a127b1662745078fa11e7cb6a220317dd96c89511c01bc4b49"
     end
   end
 
   def install
     bin.install "mongocli"
+    (bash_completion/"mongocli.sh").write `#{bin}/mongocli completion bash`
+    (zsh_completion/"_mongocli").write `#{bin}/mongocli completion zsh`
+    (fish_completion/"mongocli.fish").write `#{bin}/mongocli completion fish`
   end
 
   test do
