063c056847c3c3ee66138de21c4628922809cd7f
mcasimir <maurizio.cas@gmail.com>
2020-09-14 16:06:22 +0200

    mongosh 0.3.0

--- Formula/mongosh.rb
+++ Formula/mongosh.rb
@@ -2,13 +2,14 @@ require "language/node"
 
 class Mongosh < Formula
   desc "The MongoDB Shell"
+
   homepage "https://github.com/mongodb-js/mongosh#readme"
-  url "https://registry.npmjs.org/@mongosh/cli-repl/-/cli-repl-0.2.2.tgz"
-  version "0.2.2"
+  url "https://registry.npmjs.org/@mongosh/cli-repl/-/cli-repl-0.3.0.tgz"
+  version "0.3.0"
 
   # This is the checksum of the archive. Can be obtained with:
-  # curl -s https://registry.npmjs.org/@mongosh/cli-repl/-/cli-repl-0.2.2.tgz | shasum -a 256
-  sha256 "03f844e6fbfcf5860c0d1303bc7926a243eafc923112b5657995ff156bc33cb4"
+  # curl -s https://registry.npmjs.org/@mongosh/cli-repl/-/cli-repl-0.3.0.tgz | shasum -a 256
+  sha256 "0416de88b02a8f9b7bd960bcfef3e73bb43101f5c2ba8985463b6e275f00516a"
 
   depends_on "node@12"
 
