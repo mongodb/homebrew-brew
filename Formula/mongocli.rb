ea125e183206d64549b9e6c236c6ae42d31540cd
goreleaserbot <goreleaser@carlosbecker.com>
2020-10-08 13:19:18 +0100

    Brew formula update for mongocli version v1.7.0

--- Formula/mongocli.rb
+++ Formula/mongocli.rb
@@ -2,16 +2,16 @@
 class Mongocli < Formula
   desc "The MongoDB Command Line Interface (mongocli) is a tool for managing your MongoDB cloud services, like MongoDB Atlas, MongoDB Cloud Manager, and MongoDB Ops Manager."
   homepage "https://github.com/mongodb/mongocli"
-  version "1.6.0"
+  version "1.7.0"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/mongodb/mongocli/releases/download/v1.6.0/mongocli_1.6.0_macos_x86_64.zip"
-    sha256 "6550d9ecf9b5a86258ec324fcf37ec1bc553cff7b80eb036a11d416709195a75"
+    url "https://github.com/mongodb/mongocli/releases/download/v1.7.0/mongocli_1.7.0_macos_x86_64.zip"
+    sha256 "64af9d5dd3c8c8f037de4e8e8567b515c365922214792d15d7780f80ef8e4711"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/mongodb/mongocli/releases/download/v1.6.0/mongocli_1.6.0_linux_x86_64.tar.gz"
-      sha256 "909e6f98b80c711eab6fc993b309363cd2f9c5fd81f76b1670429540e12db31a"
+      url "https://github.com/mongodb/mongocli/releases/download/v1.7.0/mongocli_1.7.0_linux_x86_64.tar.gz"
+      sha256 "346cfd2366a863cb33ce53698cceb2e8a313d09858110c3ef3ac6b6662d94643"
     end
   end
 
