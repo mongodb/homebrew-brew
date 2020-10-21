428398ab2ec90d7b650731feaa74e9aac8e41fe0
Kevin Albertson <kevin.albertson@mongodb.com>
2020-10-21 14:22:14 -0400

    add libmongocrypt formula

---
+++ Formula/libmongocrypt.rb
@@ -0,0 +1,33 @@
+class Libmongocrypt < Formula
+  desc "C library for Client Side Encryption"
+  homepage "https://github.com/mongodb/libmongocrypt"
+  url "https://github.com/mongodb/libmongocrypt/archive/1.0.4.tar.gz"
+  sha256 "34244b473ceedd51f5885cd80ca3f818924fac4695b5cfcd5396092c726680d9"
+  license "Apache-2.0"
+
+  depends_on "cmake" => :build
+  depends_on "mongo-c-driver" => :build
+
+  def install
+    cmake_args = std_cmake_args
+    cmake_args << if build.head?
+      "-DBUILD_VERSION=1.1.0-pre"
+    else
+      "-DBUILD_VERSION=1.0.4"
+    end
+    system "cmake", ".", *cmake_args
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.c").write <<~EOS
+      #include <mongocrypt/mongocrypt.h>
+      int main() {
+        const char* version = mongocrypt_version (0);
+        return 0;
+      }
+    EOS
+    system ENV.cc, "test.c", "-L#{lib}", "-lmongocrypt", "-o", "test"
+    system "./test"
+  end
+end
