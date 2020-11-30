1948a196cf88e142e14301a7782dd407028806f2
Anna Henningsen <anna@addaleax.net>
2020-11-30 11:22:47 +0100

    mongosh 0.6.0

--- Formula/mongosh.rb
+++ Formula/mongosh.rb
@@ -4,18 +4,18 @@ class Mongosh < Formula
   desc "The MongoDB Shell"
 
   homepage "https://github.com/mongodb-js/mongosh#readme"
-  url "https://registry.npmjs.org/@mongosh/cli-repl/-/cli-repl-0.5.2.tgz"
-  version "0.5.2"
+  url "https://registry.npmjs.org/@mongosh/cli-repl/-/cli-repl-0.6.0.tgz"
+  version "0.6.0"
 
   # This is the checksum of the archive. Can be obtained with:
-  # curl -s https://registry.npmjs.org/@mongosh/cli-repl/-/cli-repl-0.5.2.tgz | shasum -a 256
-  sha256 "8702eb0ef44603f34c05e934cac9ce0adcd8596707a5ec4010746d549f825f5f"
+  # curl -s https://registry.npmjs.org/@mongosh/cli-repl/-/cli-repl-0.6.0.tgz | shasum -a 256
+  sha256 "d1ae6ac3380e5e9963c10598b65aca9f6873e3728004ae568e0b3dde2667c3f0"
 
-  depends_on "node@12"
+  depends_on "node@14"
 
   def install
-    system "#{Formula["node@12"].bin}/npm", "install", *Language::Node.std_npm_install_args(libexec)
-    (bin/"mongosh").write_env_script libexec/"bin/mongosh", :PATH => "#{Formula["node@12"].opt_bin}:$PATH"
+    system "#{Formula["node@14"].bin}/npm", "install", *Language::Node.std_npm_install_args(libexec)
+    (bin/"mongosh").write_env_script libexec/"bin/mongosh", :PATH => "#{Formula["node@14"].opt_bin}:$PATH"
   end
 
   test do
