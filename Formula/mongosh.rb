c7b55b8d923115f57e07e827921ad5dcf7611604
Durran Jordan <durran@gmail.com>
2021-02-23 10:54:37 +0100

    mongosh 0.8.1

--- Formula/mongosh.rb
+++ Formula/mongosh.rb
@@ -4,12 +4,12 @@ class Mongosh < Formula
   desc "The MongoDB Shell"
 
   homepage "https://github.com/mongodb-js/mongosh#readme"
-  url "https://registry.npmjs.org/@mongosh/cli-repl/-/cli-repl-0.8.0.tgz"
-  version "0.8.0"
+  url "https://registry.npmjs.org/@mongosh/cli-repl/-/cli-repl-0.8.1.tgz"
+  version "0.8.1"
 
   # This is the checksum of the archive. Can be obtained with:
-  # curl -s https://registry.npmjs.org/@mongosh/cli-repl/-/cli-repl-0.8.0.tgz | shasum -a 256
-  sha256 "77feadff95f548a09bec4655b5a97d990acf06674b6464ac8794aef53a1fa2fa"
+  # curl -s https://registry.npmjs.org/@mongosh/cli-repl/-/cli-repl-0.8.1.tgz | shasum -a 256
+  sha256 "10cfc51debe735884de9910de3cd7d5f1027b1bdbb1a8c3b944186d2f8562854"
 
   depends_on "node@14"
 
