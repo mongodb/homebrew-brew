3f03a158dea616a1d9b667f24bb62b0508978197
goreleaserbot <goreleaser@carlosbecker.com>
2021-04-01 14:26:51 +0100

    Brew formula update for mongocli version v1.14.0

--- Formula/mongocli.rb
+++ Formula/mongocli.rb
@@ -5,17 +5,17 @@
 class Mongocli < Formula
   desc "The MongoDB Command Line Interface (mongocli) is a tool for managing your MongoDB cloud services, like MongoDB Atlas, MongoDB Cloud Manager, and MongoDB Ops Manager."
   homepage "https://github.com/mongodb/mongocli"
-  version "1.13.0"
+  version "1.14.0"
   license "Apache-2.0"
   bottle :unneeded
 
-  if OS.mac?
-    url "https://github.com/mongodb/mongocli/releases/download/v1.13.0/mongocli_1.13.0_macos_x86_64.zip"
-    sha256 "21468eea1c8bf221113c489955a1d643191ab4d265670b36d0cc66a6c60e9516"
+  if OS.mac? && Hardware::CPU.intel?
+    url "https://github.com/mongodb/mongocli/releases/download/v1.14.0/mongocli_1.14.0_macos_x86_64.zip"
+    sha256 "2fca7ae5bd8f00468910a2a472f8c3f88aaa65ca2df4ad546b83a8d840e38c8d"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/mongodb/mongocli/releases/download/v1.13.0/mongocli_1.13.0_linux_x86_64.tar.gz"
-    sha256 "745c4266998e554fd7d94e78434aa9e231b3d6d6a7c86636e678fefe718d923d"
+    url "https://github.com/mongodb/mongocli/releases/download/v1.14.0/mongocli_1.14.0_linux_x86_64.tar.gz"
+    sha256 "368fb976113d71bfb801f88fb96d072ce318e08e104a2aff78a659fe07e2f24e"
   end
 
   def install
