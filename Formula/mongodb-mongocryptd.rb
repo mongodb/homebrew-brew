2acd7e861489ba28753e5e53642f6e6fbdcc8020
Michael Rose <michael_rose@gmx.de>
2021-05-28 11:16:40 +0200

    chore: add new formula for mongocryptd MONGOSH-800

---
+++ Formula/mongodb-mongocryptd.rb
@@ -0,0 +1,18 @@
+class MongodbMongocryptd < Formula
+  desc "mongocryptd service for Client Side Encryption"
+  homepage "https://www.mongodb.com/"
+
+  url "https://downloads.mongodb.com/osx/mongodb-cryptd-macos-x86_64-enterprise-4.4.6.tgz"
+  sha256 "bdf8aa2110b1f72c847b6b49f73fb855ae58549a2640ea69d7971740d7ff10db"
+  license "MongoDB Customer Agreement"
+
+  def caveats
+    <<~EOS
+      mongocryptd is licensed under the MongoDB Customer Agreement (https://www.mongodb.com/customer-agreement). Except for evaluation purposes, you may not use mongocryptd without a commercial license from MongoDB.
+    EOS
+  end
+
+  test do
+    system "#{bin}/mongocryptd", "--version"
+  end
+end
