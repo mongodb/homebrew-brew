4fb13d6f5225cbad05ea06bbbd9ed9a0828c0fe2
Kevin Albertson <kevin.albertson@mongodb.com>
2022-03-02 08:58:39 -0500

    update libmongocrypt to 1.3.1

--- Formula/libmongocrypt.rb
+++ Formula/libmongocrypt.rb
@@ -1,8 +1,8 @@
 class Libmongocrypt < Formula
   desc "C library for Client Side Encryption"
   homepage "https://github.com/mongodb/libmongocrypt"
-  url "https://github.com/mongodb/libmongocrypt/archive/1.3.0.tar.gz"
-  sha256 "e32550468b4be1d24c0a228a7b49cd586571391f65f434a49ab2bf736ec55d11"
+  url "https://github.com/mongodb/libmongocrypt/archive/1.3.1.tar.gz"
+  sha256 "7c1f10021e7813829fdb992fa64d8aa5e2453d6f49aced5238c4bf2d86a84535"
   license "Apache-2.0"
   head "https://github.com/mongodb/libmongocrypt.git"
 
@@ -14,7 +14,7 @@ class Libmongocrypt < Formula
     cmake_args << if build.head?
       "-DBUILD_VERSION=1.4.0-pre"
     else
-      "-DBUILD_VERSION=1.3.0"
+      "-DBUILD_VERSION=1.3.1"
     end
     system "cmake", ".", *cmake_args
     system "make", "install"
