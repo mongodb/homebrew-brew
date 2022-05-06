bb5295b96a6becc415587f2c0b0d8bc00f3d2e44
Anna Henningsen <anna.henningsen@mongodb.com>
2022-05-06 17:15:16 +0200

    fixup: inline shas

--- Formula/mongodb-csfle.rb
+++ Formula/mongodb-csfle.rb
@@ -2,18 +2,16 @@ class MongodbCsfle < Formula
   desc "MongoDB csfle shared library for Client Side Encryption"
   homepage "https://www.mongodb.com/"
 
-  sha256_aarch64 = "5e5bc88bcadf256c7dcea08a7aa55a552630de6e6919fcf3215b5e8171231c03"
-  sha256_x86_64 = "4f1043d8b07ace6032a52eb41b193b3380061a7564ce21ffae24b1ccc0e5c558"
   license "MongoDB Customer Agreement"
 
   if Hardware::CPU.arm?
     # TODO(MONGOSH-1193): Bump to production version after 6.0 is released
     url "https://downloads.mongodb.com/osx/mongo_csfle_v1-macos-arm64-enterprise-6.0.0-rc4.tgz"
-    sha256 sha256_aarch64
+    sha256 "5e5bc88bcadf256c7dcea08a7aa55a552630de6e6919fcf3215b5e8171231c03"
   else
     # TODO(MONGOSH-1193): Bump to production version after 6.0 is released
     url "https://downloads.mongodb.com/osx/mongo_csfle_v1-macos-x86_64-enterprise-6.0.0-rc4.tgz"
-    sha256 sha256_x86_64
+    sha256 "4f1043d8b07ace6032a52eb41b193b3380061a7564ce21ffae24b1ccc0e5c558"
   end
 
 
