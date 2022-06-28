07bacc1a828e07e7088dd83931fc1436dc20a83e
Kevin Albertson <kevin.albertson@mongodb.com>
2022-06-28 15:36:48 -0400

    update libmongocrypt to 1.5.0

--- Formula/libmongocrypt.rb
+++ Formula/libmongocrypt.rb
@@ -1,10 +1,10 @@
 class Libmongocrypt < Formula
   desc "C library for Client Side Encryption"
   homepage "https://github.com/mongodb/libmongocrypt"
-  url "https://github.com/mongodb/libmongocrypt/archive/1.4.1.tar.gz"
-  sha256 "15f118ac42a9df9dd86af2d93bd763bbc128977b73cfd430cb1d6270350e344e"
+  url "https://github.com/mongodb/libmongocrypt/archive/1.5.0.tar.gz"
+  sha256 "d96f0fae590e69ed37979aad27db191a9b804d4e16ea74d5ce1a16520fd1c053"
   license "Apache-2.0"
-  head "https://github.com/mongodb/libmongocrypt.git", tag: "1.5.0-rc2"
+  head "https://github.com/mongodb/libmongocrypt.git"
 
   depends_on "cmake" => :build
   depends_on "mongo-c-driver" => :build
@@ -12,9 +12,9 @@ class Libmongocrypt < Formula
   def install
     cmake_args = std_cmake_args
     cmake_args << if build.head?
-      "-DBUILD_VERSION=1.5.0-rc2"
+      "-DBUILD_VERSION=1.6.0-pre"
     else
-      "-DBUILD_VERSION=1.4.1"
+      "-DBUILD_VERSION=1.5.0"
     end
     system "cmake", ".", *cmake_args
     system "make", "install"
