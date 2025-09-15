7d84b7c792128bd1184ab8040610db88447a1c34
Kevin Albertson <kevin.albertson@mongodb.com>
2025-09-15 09:07:39 -0400

    update for 1.15.2

--- Formula/libmongocrypt.rb
+++ Formula/libmongocrypt.rb
@@ -1,8 +1,8 @@
 class Libmongocrypt < Formula
   desc "C library for Client Side Encryption"
   homepage "https://github.com/mongodb/libmongocrypt"
-  url "https://github.com/mongodb/libmongocrypt/archive/1.15.1.tar.gz"
-  sha256 "bd4cda549375abd0bd007d5764c5accc7c40bca03f2948bf6c2c0ccf03338f20"
+  url "https://github.com/mongodb/libmongocrypt/archive/1.15.2.tar.gz"
+  sha256 "fe60fc4943a73d93cb29b8b20b026b7da634614ef4b4f92d9c2f98d166594bf4"
   license "Apache-2.0"
   head "https://github.com/mongodb/libmongocrypt.git"
 
@@ -14,7 +14,7 @@ class Libmongocrypt < Formula
     cmake_args << if build.head?
       "-DBUILD_VERSION=1.16.0-pre"
     else
-      "-DBUILD_VERSION=1.15.1"
+      "-DBUILD_VERSION=1.15.2"
     end
     # Homebrew includes FETCHCONTENT_FULLY_DISCONNECTED=ON as part of https://github.com/Homebrew/brew/pull/17075
     # Set back the previous default to prevent build failure.
