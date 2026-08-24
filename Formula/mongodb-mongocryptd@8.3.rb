1c9a8ab2b77a1ec8f01b61e1c35bd1017b12dc28
Evan Fetsko <evan.fetsko@mongodb.com>
2026-08-24 16:01:20 -0400

    create @8.3 pinned formula

---
+++ Formula/mongodb-mongocryptd@8.3.rb
@@ -0,0 +1,27 @@
+class MongodbMongocryptdAT83 < Formula
+  desc "mongocryptd service for Client Side Encryption"
+  homepage "https://www.mongodb.com/"
+
+  if Hardware::CPU.intel?
+    url "https://downloads.mongodb.com/osx/mongodb-cryptd-macos-x86_64-enterprise-8.3.4.tgz"
+    sha256 "df8b2ae83fa45dde09206714d85bd53261d4879bee4d10932c2567029c546b02"
+  else
+    url "https://downloads.mongodb.com/osx/mongodb-cryptd-macos-arm64-enterprise-8.3.4.tgz"
+    sha256 "0560f860863a5250433ab5942ca46d97d743a824409b1daeed7f44840d92ed3a"
+  end
+  license "MongoDB Customer Agreement"
+
+  def caveats
+    <<~EOS
+      mongocryptd is licensed under the MongoDB Customer Agreement (https://www.mongodb.com/customer-agreement). Except for evaluation purposes, you may not use mongocryptd without a commercial license from MongoDB.
+    EOS
+  end
+
+  def install
+    prefix.install Dir["*"]
+  end
+
+  test do
+    system "#{bin}/mongocryptd", "--version"
+  end
+end
