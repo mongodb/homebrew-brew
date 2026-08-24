2546b86c91a8671da2710ef05c64d084fff68ad4
Evan Fetsko <evan.fetsko@mongodb.com>
2026-08-24 16:01:20 -0400

    create @8.3 pinned formula

---
+++ Formula/mongodb-community@8.3.rb
@@ -0,0 +1,71 @@
+class MongodbCommunityAT83 < Formula
+  desc "High-performance, schema-free, document-oriented database"
+  homepage "https://www.mongodb.com/"
+
+  # frozen_string_literal: true
+
+  if Hardware::CPU.intel?
+    url "https://fastdl.mongodb.org/osx/mongodb-macos-x86_64-8.3.4.tgz"
+    sha256 "a3090f08980a884f16793c8fa3bc58f501c47e5ab756b65f07b5051b487ed66e"
+  else
+    url "https://fastdl.mongodb.org/osx/mongodb-macos-arm64-8.3.4.tgz"
+    sha256 "ef2487f688c6fa544a97b6aed63d939fd664de80781b250ad84a523cc5604273"
+  end
+
+  option "with-enable-test-commands", "Configures MongoDB to allow test commands such as failpoints"
+
+  depends_on "mongodb-database-tools" => :recommended
+  depends_on "mongosh" => :recommended
+
+  conflicts_with "mongodb-enterprise"
+
+  def install
+    inreplace "macos_mongodb.plist" do |s|
+      s.gsub!("\#{plist_name}", "#{plist_name}")
+      s.gsub!("\#{opt_bin}", "#{opt_bin}")
+      s.gsub!("\#{etc}", "#{etc}")
+      s.gsub!("\#{HOMEBREW_PREFIX}", "#{HOMEBREW_PREFIX}")
+      s.gsub!("\#{var}", "#{var}")
+    end
+
+    prefix.install Dir["*"]
+    prefix.install_symlink "macos_mongodb.plist" => "#{plist_name}.plist"
+  end
+
+  def post_install
+    (var/"mongodb").mkpath
+    (var/"log/mongodb").mkpath
+    if !(File.exist?((etc/"mongod.conf"))) then
+      (etc/"mongod.conf").write mongodb_conf
+    end
+  end
+
+  service do
+    name macos: "#{plist_name}"
+  end
+
+  def mongodb_conf
+    cfg = <<~EOS
+    systemLog:
+      destination: file
+      path: #{var}/log/mongodb/mongo.log
+      logAppend: true
+    storage:
+      dbPath: #{var}/mongodb
+    net:
+      bindIp: 127.0.0.1, ::1
+      ipv6: true
+    EOS
+    if build.with? "enable-test-commands"
+      cfg += <<~EOS
+      setParameter:
+        enableTestCommands: 1
+      EOS
+    end
+    cfg
+  end
+
+  test do
+    system "#{bin}/mongod", "--sysinfo"
+  end
+end
